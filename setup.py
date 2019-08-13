d7b1b435ea7da1ccf79a1dee4f1911686277d91f
Rinck H. Sonnenberg <r.sonnenberg@netson.nl>
2019-08-13 22:01:30 +0200

    initial version

--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
 from distutils.core import setup
 setup(
-  name = 'ahvl',                          # How you named your package folder (MyLib)
-  packages = ['ahvl'],                    # Chose the same as "name"
-  version = '0.2',                              # Start with a small number and increase it with every change you make
-  license = 'MIT',                              # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Base libraries for the Netson HashiCorp Vault Lookup Plugin for Ansible',   # Give a short description about your library
-  author = 'RH Sonnenberg',                     # Type in your name
-  author_email = 'r.sonnenberg@netson.nl',      # Type in your E-Mail
-  url = 'https://github.com/netson/ahvl', # Provide either the link to your github or to your website
-  download_url = 'https://github.com/netson/ahvl/archive/v0_2.tar.gz',    # I explain this later on
+  name = 'ahvl',
+  packages = ['ahvl'],
+  version = '0.3',
+  license = 'MIT',                                  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Base libraries for the Netson HashiCorp Vault Lookup Plugin for Ansible',
+  author = 'RH Sonnenberg',
+  author_email = 'r.sonnenberg@netson.nl',
+  url = 'https://github.com/netson/ahvl',           # Provide either the link to your github or to your website
+  download_url = 'https://github.com/netson/ahvl/archive/v0_3.tar.gz',    # I explain this later on
   keywords = ['ansible', 'hashicorp', 'vault', 'lookup'],   # Keywords that define your package best
-  install_requires=[                            # I get to this in a second
-          'os',
-          're',
-          'time',
-          'distutils',
-          'passlib',
-      ],
+  install_requires=['passlib'],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',              # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',              # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',       # Again, pick a license
+    'Programming Language :: Python :: 3',          # Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
