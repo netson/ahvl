714d7caac428f6ed0adad56610d6270a044c91f2
Rinck H. Sonnenberg <r.sonnenberg@netson.nl>
2019-08-16 13:44:41 +0200

    more fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-import setuptools
+from setuptools import setup, find_packages
+from os import path
+from io import open
 
-with open("DESCRIPTION.md", "r") as fh:
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'DESCRIPTION.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(".md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="ahvl",
-    version="1.0.1",
+    version="1.0.2",
     author="RH Sonnenberg",
     author_email="r.sonnenberg@netson.nl",
     description="Base libraries for the Ansible HashiCorp Vault Lookup (AHVL) Plugin by Netson",
@@ -13,8 +21,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     license='MIT',
     url="https://github.com/netson/ahvl",
-    download_url='https://github.com/netson/ahvl/archive/v1.0.1.tar.gz',
-    packages=setuptools.find_packages(),
+    download_url='https://github.com/netson/ahvl/archive/v1.0.2.tar.gz',
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
