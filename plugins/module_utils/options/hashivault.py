5fd0bf8f1696e83242dc74b363936c867bf55d18
gardar <gardar@users.noreply.github.com>
2022-02-11 12:35:40 +0000

    Fix lint

--- plugins/module_utils/options/hashivault.py
+++ plugins/module_utils/options/hashivault.py
@@ -1,8 +1,10 @@
 #
 # import modules
 #
-from ansible_collections.netson.ahvl.plugins.module_utils.options.base import OptionsBase
-from ansible_collections.netson.ahvl.plugins.module_utils.helper import AhvlMsg, AhvlHelper
+from ansible_collections.netson.ahvl.plugins.module_utils.options.base \
+    import OptionsBase
+from ansible_collections.netson.ahvl.plugins.module_utils.helper \
+    import AhvlMsg, AhvlHelper
 import os
 
 #
@@ -14,6 +16,8 @@ hlp = AhvlHelper()
 #
 # OptionsHashiVault
 #
+
+
 class OptionsHashiVault(OptionsBase):
 
     # set option prefix
@@ -21,7 +25,6 @@ class OptionsHashiVault(OptionsBase):
 
         # return option prefix
         return "ahvl_connection"
-
 
     # set path
     # useable variables:
@@ -41,26 +44,24 @@ class OptionsHashiVault(OptionsBase):
             vtkn = None
         # set default option values - dict
         return {
-            'ahvl_url'             : 'http://localhost:8200',    # hashi vault url i.e. https://fqdn:8200
-            'ahvl_auth_method'     : 'token',                    # authentication method
-            'ahvl_namespace'       : None,                       # secret namespace
-            'ahvl_validate_certs'  : True,                       # validate vault certificates
-            'ahvl_mount_point'     : 'secret',                   # vault secret mount point
-            'ahvl_cacert'          : None,                       # vault login certificate
-            'ahvl_username'        : None,                       # vault login username
-            'ahvl_password'        : None,                       # vault login password
-            'ahvl_role_id'         : None,                       # vault login role id
-            'ahvl_secret_id'       : None,                       # vault login secret id
-            'ahvl_token'           : vtkn,                       # vault token
+            'ahvl_url': 'http://localhost:8200',  # vault url
+            'ahvl_auth_method': 'token',  # authentication method
+            'ahvl_namespace': None,  # secret namespace
+            'ahvl_validate_certs': True,  # validate vault cert
+            'ahvl_mount_point': 'secret',  # vault secret mount point
+            'ahvl_cacert': None,  # vault login certificate
+            'ahvl_username': None,  # vault login username
+            'ahvl_password': None,  # vault login password
+            'ahvl_role_id':  None,  # vault login role id
+            'ahvl_secret_id': None,  # vault login secret id
+            'ahvl_token': vtkn,  # vault token
         }
-
 
     # calculate any remaining options
     def get_appended(self):
 
         # return list of overide options or calculated options
         return {}
-
 
     # set required options
     def get_required(self):
@@ -71,7 +72,6 @@ class OptionsHashiVault(OptionsBase):
             'ahvl_auth_method',
         ]
 
-
     # validate all set options
     def validate(self):
 
@@ -80,13 +80,20 @@ class OptionsHashiVault(OptionsBase):
 
         # sanity checks
         if o['ahvl_auth_method'] == 'token' and hlp.isempty(o['ahvl_token']):
-            msg.fail("you have selected token authentication for vault, but the token is missing", o)
+            msg.fail("you have selected token authentication for vault, \
+                but the token is missing", o)
 
         # user/pass for ldap and userpass authentication
-        if (o['ahvl_auth_method'] == "ldap" or o['ahvl_auth_method'] == 'userpass') \
-        and (hlp.isempty(o['ahvl_username']) or hlp.isempty(o['ahvl_password'])):
-            msg.fail("authentication method [{}] requires a username and password".format(o['ahvl_auth_method']), o)
+        if (o['ahvl_auth_method'] == "ldap"
+            or o['ahvl_auth_method'] == 'userpass') \
+                and (hlp.isempty(o['ahvl_username'])
+                     or hlp.isempty(o['ahvl_password'])):
+            msg.fail("authentication method [{}] requires \
+                a username and password".format(o['ahvl_auth_method']), o)
 
         # role_id/secret_id for approle authentication
-        if o['ahvl_auth_method'] == 'approle' and (hlp.isempty(o['ahvl_role_id']) or hlp.isempty(o['ahvl_secret_id'])):
-            msg.fail("authentication method app role requires a role_id and secret_id", o)
+        if o['ahvl_auth_method'] == 'approle' \
+                and (hlp.isempty(o['ahvl_role_id'])
+                     or hlp.isempty(o['ahvl_secret_id'])):
+            msg.fail("authentication method app role requires a \
+                role_id and secret_id", o)
